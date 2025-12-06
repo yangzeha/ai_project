4a612c2fba8d3392c9f423488c47964448c6f093
yangzeha <2785602345@qq.com>
2025-12-06 10:45:50 +0800

    Update Kaggle notebook to use repo files directly

--- TSB_CL_Project/gen_notebook.py
+++ TSB_CL_Project/gen_notebook.py
@@ -25,56 +25,15 @@ def generate_notebook():
         "!chmod +x Similar-Biclique-Idx-main/msbe\n"
     ]
     
-    # Cell 3: Prepare Data Script (Using Python write instead of magic)
-    # Read local file content
-    with open(r"c:\Users\LENOVO\Desktop\论文代码\ai_project\TSB_CL_Project\prepare_yelp2018.py", "r", encoding="utf-8") as f:
-        prepare_content = f.read()
-    
-    # Escape backslashes to prevent them from being interpreted as escape sequences in the python string
-    prepare_content = prepare_content.replace('\\', '\\\\')
-    # Escape triple quotes
-    prepare_content = prepare_content.replace('"""', '\\"\\"\\"')
-    
+    # Cell 3: Run Prepare
     source_3 = [
-        "# [3] Create Data Preparation Script\n",
-        "import os\n",
-        "code = \"\"\"\n"
-    ] + [line + "\n" for line in prepare_content.splitlines()] + [
-        "\"\"\"\n",
-        "os.makedirs('TSB_CL_Project', exist_ok=True)\n",
-        "with open('TSB_CL_Project/prepare_yelp2018.py', 'w', encoding='utf-8') as f:\n",
-        "    f.write(code)\n"
-    ]
-    
-    # Cell 4: Run Prepare
-    source_4 = [
-        "# [4] Download & Process Yelp2018\n",
+        "# [3] Download & Process Yelp2018\n",
         "!python TSB_CL_Project/prepare_yelp2018.py\n"
     ]
     
-    # Cell 5: Proof Script (Using Python write instead of magic)
-    with open(r"c:\Users\LENOVO\Desktop\论文代码\ai_project\TSB_CL_Project\quick_proof_yelp.py", "r", encoding="utf-8") as f:
-        proof_content = f.read()
-        
-    # Escape backslashes
-    proof_content = proof_content.replace('\\', '\\\\')
-    # Escape triple quotes
-    proof_content = proof_content.replace('"""', '\\"\\"\\"')
-        
-    source_5 = [
-        "# [5] Create Quick Proof Script\n",
-        "import os\n",
-        "code = \"\"\"\n"
-    ] + [line + "\n" for line in proof_content.splitlines()] + [
-        "\"\"\"\n",
-        "os.makedirs('TSB_CL_Project', exist_ok=True)\n",
-        "with open('TSB_CL_Project/quick_proof_yelp.py', 'w', encoding='utf-8') as f:\n",
-        "    f.write(code)\n"
-    ]
-    
-    # Cell 6: Run Proof
-    source_6 = [
-        "# [6] Run Quick Proof (Static TSB-CL vs LightGCN)\n",
+    # Cell 4: Run Proof
+    source_4 = [
+        "# [4] Run Quick Proof (4-Model Comparison)\n",
         "!python TSB_CL_Project/quick_proof_yelp.py\n"
     ]
     
@@ -84,9 +43,7 @@ def generate_notebook():
             {"cell_type": "code", "execution_count": None, "metadata": {}, "outputs": [], "source": source_1},
             {"cell_type": "code", "execution_count": None, "metadata": {}, "outputs": [], "source": source_2},
             {"cell_type": "code", "execution_count": None, "metadata": {}, "outputs": [], "source": source_3},
-            {"cell_type": "code", "execution_count": None, "metadata": {}, "outputs": [], "source": source_4},
-            {"cell_type": "code", "execution_count": None, "metadata": {}, "outputs": [], "source": source_5},
-            {"cell_type": "code", "execution_count": None, "metadata": {}, "outputs": [], "source": source_6}
+            {"cell_type": "code", "execution_count": None, "metadata": {}, "outputs": [], "source": source_4}
         ],
         "metadata": {
             "kernelspec": {
